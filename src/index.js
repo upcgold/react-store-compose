27dbf3efba4de0d32ce3dbe14ce70a0492af046d
Your Name <you@example.com>
2020-12-12 10:18:13 -0800

    adding css file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Client from 'shopify-buy';
-import '../../shared/app.css';
+import './app.css';
 
 const client = Client.buildClient({
   storefrontAccessToken: 'c0ec5eab45c3f5a5041892ef47334493',
